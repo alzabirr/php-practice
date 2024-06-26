85553446b18eb67f5fcc3975458b0e1851a0142f
alzabirr <alzabirhussain@gmail.com>
2024-06-26 22:22:52 +0600

    odd/even

--- odd-number.php
+++ odd-number.php
@@ -3,7 +3,7 @@
 $number1= 11;
 
 if($number1 % 2 == 0){
-    echo"this is enven number ",$number1;
+    echo"this is enen number ",$number1;
 }
 else{
     echo"this is odd number ",$number1;
--- str.php
+++ str.php
@@ -18,6 +18,14 @@ printf("hi i am %s %s", $first_name, $last_name);
  echo $output;
 //  
 
+// leapyear 
+$year = 24;
+if ($year % 4 == 0){
+    printf("this is leapyear %d", $year);
+}
+else{
+    printf("this is not leapyear %d", $year);
+}
 
 
 
