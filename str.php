b6fef48cbc118d31541e7d4d434da34a81f40b40
alzabirr <alzabirhussain@gmail.com>
2024-06-26 15:41:32 +0600

    vs

--- str.php
+++ str.php
@@ -5,12 +5,14 @@
 $first_name = "Al";
 $last_name = "Zabir";
 
-// printf function 
-// printf("hi i am %s %s", $first_name, $last_name); 
+// printf function
+
+printf("hi i am %s %s", $first_name, $last_name); 
 // 
 
 // sprintf output  kre na just return kre
 
+// printf VS sprintf 
  $output =  sprintf("hi i am %s %s", $first_name, $last_name);
 
  echo $output;
